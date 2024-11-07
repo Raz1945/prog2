fe6ee803e095a4a394a880c1773d4a36c14ebdc4
Raz1945 <g.sebas.sanchez@gmail.com>
2024-11-07 14:17:01 -0300

    Creacion y Fin de TEnvio

--- Tarea4/src/envio.cpp
+++ Tarea4/src/envio.cpp
@@ -1,17 +1,46 @@
 #include "../include/envio.h"
 
 struct rep_envio {
+  TCarritoProductos carrito;
+  TFecha fecha;
+  rep_envio* sig;
+
 };
 
 TEnvio crearTEnvio(TCarritoProductos carrito, TFecha fecha) {
-  return NULL;
+  TEnvio nuevo = new rep_envio;
+  nuevo->carrito = carrito;
+  nuevo->fecha = fecha;
+  nuevo->sig = NULL;
+
+  return nuevo;
 }
 
-void liberarTEnvio(TEnvio &envio) {}
+void liberarTEnvio(TEnvio &envio) {
+  TEnvio aBorrar;
+  while (envio != NULL) {
+    aBorrar = envio;
+    envio = envio->sig;
+    liberarCarritoProductos(aBorrar->carrito);
+    liberarTFecha(aBorrar->fecha);
+    delete aBorrar;
+  }
 
-TCarritoProductos obtenerCarritoProductosTEnvio(TEnvio envio) { return NULL; }
+  envio = NULL;
+}
 
-TFecha obtenerFechaTEnvio(TEnvio envio) { return NULL; }
+TCarritoProductos obtenerCarritoProductosTEnvio(TEnvio envio) {
+  return envio->carrito;
+}
+
+TFecha obtenerFechaTEnvio(TEnvio envio) {
+   return envio->fecha;
+}
 
 void imprimirTEnvio(TEnvio envio) {
+    imprimirCarritoProductos(envio->carrito);
+    printf("\n");
+    printf("Fecha del envio: ");
+    imprimirTFecha(envio->fecha);
+    printf("\n");
 }
